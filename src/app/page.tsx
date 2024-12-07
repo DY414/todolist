c4b3620cd0c1ba5522fc5fa1b5d29d07bde29f36
DY414 <tmvldghkfur@gmail.com>
2024-12-08 01:53:17 +0900

    - add global css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import TodoUserListPage from "../components/pages/TodoUserListPage";
 import { useRecoilValue } from "recoil";
 import { todoListState } from "../atom/todoAtom";
 
+import '../components/styles/Global.css';
 
 const Page = () => {
   const todoList = useRecoilValue(todoListState);
